b47e1d772c638bd21a6cb67a9d7de64e6b95bf11
cerentkn04 <cerentkn04@gmail.com>
2026-01-31 19:27:24 +0300

    coyote jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,96 +2,89 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
-  public PlayerController p;
-  public CharacterController pc;
+    public PlayerController p;
+    public CharacterController pc;
 
-  private Vector3 grvty;
-  private float G = -9.8f * 2.0f;
+    private Vector3 grvty;
+    private float G = -9.8f * 2.0f;
 
-  private float sprinting_speed = 5f;
-  private float move_speed = 3.5f;
+    private float sprinting_speed = 5f;
+    private float move_speed = 3.5f;
+    private float current_speed;
 
-  private float current_speed;
-  private bool is_grouneded;
-  
-  public float jump_height = 2;
-  public float gravity_scale = 5;
+    private bool is_grounded;
 
+    public float jump_height = 2;
+    public float gravity_scale = 5;
 
-  private float xRotation;
-  public float xSensitivity = 60;
-  public float ySensitivity = 60;
+    private float xRotation;
+    public float xSensitivity = 60;
+    public float ySensitivity = 60;
 
-  public Camera cam;
+    public Camera cam;
+    private bool sprinting = false;
 
-  private bool sprinting = false;
+    public float coyoteTime = 0.3f;
+    private float coyoteTimer;
 
-  private Rigidbody held_object = null;
-  private float held_distance = 1;
+    private bool jumpRequested = false;
 
-  int layer7Mask = 1 << 7;
-  
-  void Awake(){
-    p = new PlayerController();
-    pc = GetComponent<CharacterController>();
-  }
-  void OnEnable(){
-    p.Enable();
-  }
-  void OnDisable(){
-    p.Disable();
-  }
-
-
-  void FixedUpdate(){
-    Vector3 velocity = Vector3.zero;
-
-    velocity.x = p.Player.Move.ReadValue<Vector2>().x;
-    velocity.z = p.Player.Move.ReadValue<Vector2>().y;
-    Vector3 camForward = cam.transform.forward;
-    Vector3 camRight = cam.transform.right;
-
-    camForward.y = 0;
-    camRight.y = 0;
-    camForward.Normalize();
-    camRight.Normalize();
-    Vector3 moveDirection = (camForward * velocity.z) + (camRight * velocity.x);
-
-    pc.Move(moveDirection * current_speed * Time.deltaTime);
-    grvty.y += G * Time.deltaTime;
-    if(is_grouneded && grvty.y < 0){
-      grvty.y = -2;
-    }
-    pc.Move(grvty * Time.deltaTime);
-
-  }
-
-
-  void LateUpdate(){
-    float mouseX = p.Player.Look.ReadValue<Vector2>().x;
-    float mouseY = p.Player.Look.ReadValue<Vector2>().y;
-
-    xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
-    xRotation = Mathf.Clamp(xRotation, -80, 80);
-
-    transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
-  }
-
-  void Update()
-  {
-    current_speed = move_speed;
-    if(sprinting){
-      current_speed = sprinting_speed;
+    void Awake()
+    {
+        p = new PlayerController();
+        pc = GetComponent<CharacterController>();
     }
 
-    is_grouneded = pc.isGrounded;
-    float jump_force = Mathf.Sqrt(jump_height * -3 * (Physics.gravity.y * gravity_scale));
-    if(p.Player.Jump.triggered){
-      if(is_grouneded){
-        grvty.y = Mathf.Sqrt(jump_height * -3.0f * G);
-      }
+    void OnEnable() => p.Enable();
+    void OnDisable() => p.Disable();
+
+    void Update()
+    {
+        current_speed = sprinting ? sprinting_speed : move_speed;
+
+        is_grounded = pc.isGrounded;
+        if (is_grounded)
+            coyoteTimer = coyoteTime;
+        else
+            coyoteTimer -= Time.deltaTime;
+        if (p.Player.Jump.triggered)
+            jumpRequested = true;
     }
-  }
+
+    void FixedUpdate()
+    {
+        Vector2 input = p.Player.Move.ReadValue<Vector2>();
+        Vector3 camForward = cam.transform.forward;
+        Vector3 camRight = cam.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 moveDir = camForward * input.y + camRight * input.x;
+        pc.Move(moveDir * current_speed * Time.fixedDeltaTime);
+
+        if (jumpRequested && (is_grounded || coyoteTimer > 0f))
+        {
+            grvty.y = Mathf.Sqrt(jump_height * -2f * G);
+            jumpRequested = false;
+            coyoteTimer = 0f;
+        }
+
+        grvty.y += G * Time.fixedDeltaTime;
+
+        if (is_grounded && grvty.y < 0)
+            grvty.y = -2;
+
+        pc.Move(grvty * Time.fixedDeltaTime);
+    }
+
+    void LateUpdate()
+    {
+        Vector2 look = p.Player.Look.ReadValue<Vector2>();
+        xRotation -= look.y * ySensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        transform.Rotate(Vector3.up * look.x * xSensitivity * Time.deltaTime);
+    }
 }
-
